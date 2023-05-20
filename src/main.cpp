9a01c12bba52ee538528883ac3f43e13f483057a
feipiao594 <445785526@qq.com>
2023-05-20 18:50:41 +0800

    create Ocr branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <QtWidgets/QApplication>
 #include "mainui.h"
+#include "test.h"
+
 
 int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    new MainUI;
-    return a.exec();
+    //QApplication a(argc, argv);
+    //QTextCodec* wordtype = QTextCodec::codecForName("UTF-8");
+
+    test();
+    //new MainUI;
+    //return a.exec();
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,35 @@
-#include "test.h"
-#include "ocr.h"
 #include <QAxWidget>
 #include <QPdfDocument>
+#include <QBuffer>
+
+#include "test.h"
+#include "ocr.h"
 
 std::string test() {
     std::string ak = "7b5055c0ecfc4d19b8def6869898fc8b";
     std::string sk = "33df542ec17d417a80d0a56fdcb1118c";
     cmssai::Ocr client(ak, sk);
 
+
+    //std::string idcard_image_url =
+    // "https://ai.bdstatic.com/file/D348C98B677149C8B95909CFA204BA1C";
+    //QString result = QString::fromStdString(client.idcard_url(idcard_image_url, cmssai::null));
+    //std::cout<<idcard_result<<std::endl;
+    //qDebug()<<result.toStdString().c_str();
+
     std::map<std::string, std::string> options;
-    options.emplace(std::make_pair("pdf_page_index", "0"));
-    options.emplace(std::make_pair("enable_pdf_recognize", "true"));
+    QImage image("C:/Users/feipiao/Desktop/1_00");
+    QByteArray ba;
+    QBuffer buf(&ba);
+    image.save(&buf, "png");
+    std::string teststr = ba.toBase64().toStdString();
+    buf.close();
+    options.emplace(std::make_pair("image", teststr));
     //options.emplace("item_names", "errorcode|errormsg|session_id|recognize_list");
 
-    std::string result = client.custom_url("R(D:/softbei/pdf/1.pdf)",
-        "1", 
-        cmssai::null);
-    return result;
+    QString result = QString::fromStdString(client.generic_url(" ",
+        options));
+    qDebug()<<result.toStdString().c_str();
+    return result.toStdString();
 }
+//
