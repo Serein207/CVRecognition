440a3a25b10db178ce1491ff405760a870ab0fab
feipiao594 <445785526@qq.com>
2023-07-01 16:29:34 +0800

    新增:未测试的Cmss::getKeywordsJson

--- src/cmssinterface.cpp
+++ src/cmssinterface.cpp
@@ -3,6 +3,7 @@
 #include "nlp.h"
 #include <map>
 #include <utility>
+#include <QVector>
 
 QString CmssInterface::getGenericResultJson(const std::string& imageBase64StdStr) {
     std::map<std::string, std::string> options;
@@ -37,4 +38,16 @@ QString CmssInterface::getSegmentationJson(const QString& content) {
     entityItem.setText(contentRemoveSpace.toUtf8().toStdString());
     entityParams.push_back(entityItem);
     return QString::fromStdString(client.segmentation(entityParams));
+}
+
+QString CmssInterface::getKeywordsJson(const QVector<QString>& contentVec) {
+    cmssai::Nlp client(Ak, Sk);
+    std::vector<cmssai::Item> entityParams;
+    for(int i = 0;i < contentVec.size(); i++){
+        cmssai::Item entityItem;
+        entityItem.setTextId(std::to_string(i));
+        entityItem.setText(contentVec[i].toStdString());
+        entityParams.push_back(entityItem);
+    }
+    return QString::fromStdString(client.keywords(entityParams));
 }
